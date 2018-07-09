b56037130f5551c503104787cfb2aa23df414999
Vishal Sharma <vsh1819@gmail.com>
2018-07-09 21:27:44 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,21 +4,7 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
   
-    # def initialize
-    # self.column_names.each do |col_name|
-    #     attr_accessor col_name.to_sym
-    # end
-    # end
     
-    # def initialize(options={})
-    # options.each do |property, value|
-    #   self.send("#{property}=", value)
-    # end
-    def initialize
-      self.class.column_names.each do |col_name|
-        attr_accessor col_name.to_sym
-    end
-  end
     
     
 end
