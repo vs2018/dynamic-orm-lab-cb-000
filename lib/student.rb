b07e45dd264f692073faa7e3db48ec2f68c190c7
Vishal Sharma <vsh1819@gmail.com>
2018-07-09 21:40:15 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,18 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-
+  
+  self.column_names.each do |col_name|
+    attr_accessor col_name.to_sym
+  end
+  
+  
+    def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+    
+    
+    
 end
