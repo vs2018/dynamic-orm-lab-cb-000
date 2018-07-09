7bdac4e1e30b09a3eebb6acb63ea8da672a8478a
Vishal Sharma <vsh1819@gmail.com>
2018-07-09 21:38:16 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -48,7 +48,7 @@ class InteractiveRecord
   end
   
   def self.find_by(options)
-    sql = "SELECT * FROM #{self.table_name} WHERE #{options} = '#{name}'"
+    sql = "SELECT * FROM #{self.table_name} WHERE #{send(option)} = '#{name}'"
     DB[:conn].execute(sql)
   end
   
